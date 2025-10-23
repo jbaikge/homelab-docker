8df3594b4ceae952ac286293cbf74ed01f87f77b
jake <523216+jbaikge@users.noreply.github.com>
2025-10-23 15:13:12 -0400

    refactor: bump IPs up by 1
    - Making room for 5th NIC in case we get 2.5G cards

--- os/hosts/cherry/default.nix
+++ os/hosts/cherry/default.nix
@@ -7,19 +7,19 @@
     hostName = "cherry";
     interfaces.enp0s2f0.ipv4.addresses = [
       {
-        address = "10.100.6.42";
+        address = "10.100.6.43";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f1.ipv4.addresses = [
       {
-        address = "10.100.10.43";
+        address = "10.100.10.44";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f2.ipv4.addresses = [
       {
-        address = "192.168.1.44";
+        address = "192.168.1.45";
         prefixLength = 24;
       }
     ];
--- os/hosts/maple/default.nix
+++ os/hosts/maple/default.nix
@@ -7,19 +7,19 @@
     hostName = "maple";
     interfaces.enp0s2f0.ipv4.addresses = [
       {
-        address = "10.100.6.50";
+        address = "10.100.6.53";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f1.ipv4.addresses = [
       {
-        address = "10.100.10.51";
+        address = "10.100.10.54";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f2.ipv4.addresses = [
       {
-        address = "192.168.1.52";
+        address = "192.168.1.55";
         prefixLength = 24;
       }
     ];
