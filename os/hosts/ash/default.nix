3e360bcabc5dce3ec0ea35d5d220a9d8758b8189
jake <523216+jbaikge@users.noreply.github.com>
2025-11-08 21:10:07 -0500

    refactor: update IP addresses
    - Using /29 segments to apply last-octet numbers across different
      networks

--- os/hosts/ash/default.nix
+++ os/hosts/ash/default.nix
@@ -7,19 +7,19 @@
     hostName = "ash";
     interfaces.enp0s2f0.ipv4.addresses = [
       {
-        address = "10.100.6.38";
+        address = "10.100.6.40";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f1.ipv4.addresses = [
       {
-        address = "10.100.10.39";
+        address = "10.100.10.41";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f2.ipv4.addresses = [
       {
-        address = "192.168.1.40";
+        address = "192.168.1.42";
         prefixLength = 24;
       }
     ];
--- os/hosts/maple/default.nix
+++ os/hosts/maple/default.nix
@@ -7,19 +7,19 @@
     hostName = "maple";
     interfaces.enp0s2f0.ipv4.addresses = [
       {
-        address = "10.100.6.53";
+        address = "10.100.6.64";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f1.ipv4.addresses = [
       {
-        address = "10.100.10.54";
+        address = "10.100.10.65";
         prefixLength = 24;
       }
     ];
     interfaces.enp0s2f2.ipv4.addresses = [
       {
-        address = "192.168.1.55";
+        address = "192.168.1.66";
         prefixLength = 24;
       }
     ];
