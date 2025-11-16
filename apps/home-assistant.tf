3cae0d6de7fe83d2a468826f9d5bf6b42ee094f3
jake <523216+jbaikge@users.noreply.github.com>
2025-11-16 10:03:24 -0500

    feat: add home-assistant

---
+++ apps/home-assistant.tf
@@ -0,0 +1,64 @@
+resource "docker_image" "home_assistant" {
+  provider     = docker.hosts[var.apps.home_assistant]
+  name         = "ghcr.io/home-assistant/home-assistant:2025.11.2"
+  keep_locally = false
+}
+
+resource "docker_volume" "home_assistant_config" {
+  provider = docker.hosts[var.apps.home_assistant]
+  name     = "home-assistant"
+  driver   = "local"
+
+  driver_opts = {
+    type   = "nfs"
+    o      = "addr=${var.hosts[var.apps.home_assistant].nfs_host},rw,nfsvers=4"
+    device = ":/mnt/tank/nfs/vols/home-assistant"
+  }
+}
+
+resource "docker_container" "home_assistant" {
+  provider = docker.hosts[var.apps.home_assistant]
+  name     = "home-assistant"
+  image    = docker_image.home_assistant.image_id
+
+  labels {
+    label = "traefik.enable"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.routers.home_assistant.rule"
+    value = "Host(`home.${data.sops_file.secrets.data["domain.tld"]}`)"
+  }
+
+  labels {
+    label = "traefik.http.routers.home_assistant.entrypoints"
+    value = "websecure"
+  }
+
+  labels {
+    label = "traefik.http.routers.home_assistant.tls"
+    value = "true"
+  }
+
+  labels {
+    label = "traefik.http.services.home_assistant.loadbalancer.server.port"
+    value = "8123"
+  }
+
+  networks_advanced {
+    name = docker_network.home_assistant.id
+  }
+
+  volumes {
+    container_path = "/config"
+    volume_name    = docker_volume.home_assistant_config.name
+    read_only      = false
+  }
+
+  volumes {
+    container_path = "/etc/localtime"
+    host_path      = "/etc/localtime"
+    read_only      = true
+  }
+}
