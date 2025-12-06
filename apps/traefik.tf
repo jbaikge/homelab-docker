fe737dd8b669a270d2deeea17f48652b06ae6908
jake <523216+jbaikge@users.noreply.github.com>
2025-12-06 07:33:36 -0500

    chore(traefik): upgrade to v3.6.4

--- apps/traefik.tf
+++ apps/traefik.tf
@@ -1,7 +1,7 @@
 resource "docker_image" "traefik" {
   for_each     = toset(var.apps.traefik)
   provider     = docker.hosts[each.key]
-  name         = "traefik:v3.6.2"
+  name         = "traefik:v3.6.4"
   keep_locally = false
 }
 
