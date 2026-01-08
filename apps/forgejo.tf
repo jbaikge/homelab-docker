58827ccb40b6721a4c115cb3507c5164f2467e46
jake <523216+jbaikge@users.noreply.github.com>
2026-01-08 08:14:42 -0500

    chore(forgejo): upgrade to v13.0.4

--- apps/forgejo.tf
+++ apps/forgejo.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "forgejo" {
   provider     = docker.hosts[var.apps.forgejo]
-  name         = "codeberg.org/forgejo/forgejo:13.0.3"
+  name         = "codeberg.org/forgejo/forgejo:13.0.4"
   keep_locally = false
 }
 
