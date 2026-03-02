cc1964bf4f8dbcf7d44d2c7def5d1ba72bf00630
jake <523216+jbaikge@users.noreply.github.com>
2026-03-02 08:30:51 -0500

    chore(wakapi): upgrade to v2.17.2

--- apps/wakapi.tf
+++ apps/wakapi.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "wakapi" {
   provider     = docker.hosts[var.apps.wakapi]
-  name         = "ghcr.io/muety/wakapi:2.16.1"
+  name         = "ghcr.io/muety/wakapi:2.17.2"
   keep_locally = false
 }
 
