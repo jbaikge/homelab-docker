a7831c5b1255c98bec7031b0d82969e9494dafc8
jake <523216+jbaikge@users.noreply.github.com>
2026-03-29 09:12:00 -0400

    chore(zwave): upgrade to v11.15.1

--- apps/zwave.tf
+++ apps/zwave.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "zwave" {
   provider     = docker.hosts[var.apps.zwave]
-  name         = "zwavejs/zwave-js-ui:11.10.1"
+  name         = "zwavejs/zwave-js-ui:11.15.1"
   keep_locally = false
 }
 
