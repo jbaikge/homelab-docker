055f2a744cbbc8d67e002711a4040adc674aa678
jake <523216+jbaikge@users.noreply.github.com>
2026-03-29 09:21:41 -0400

    chore(miniflux): upgrade to v2.2.18

--- apps/miniflux.tf
+++ apps/miniflux.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "miniflux" {
   provider     = docker.hosts[var.apps.miniflux]
-  name         = "ghcr.io/miniflux/miniflux:2.2.16"
+  name         = "ghcr.io/miniflux/miniflux:2.2.18"
   keep_locally = false
 }
 
