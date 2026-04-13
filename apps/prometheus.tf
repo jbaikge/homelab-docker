5146de436516f8d365876531d998463a3afd5c2c
jake <523216+jbaikge@users.noreply.github.com>
2026-04-13 16:39:22 -0400

    chore(prometheus): upgrade to v3.11.2

--- apps/prometheus.tf
+++ apps/prometheus.tf
@@ -1,6 +1,6 @@
 resource "docker_image" "prometheus" {
   provider     = docker.hosts[var.apps.prometheus]
-  name         = "docker.io/prom/prometheus:v3.10.0"
+  name         = "docker.io/prom/prometheus:v3.11.2"
   keep_locally = false
 }
 
